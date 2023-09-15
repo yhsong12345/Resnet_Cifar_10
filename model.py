2ed7cf953ccc698ea202f3d3c893d66d3e3ceb60
yhsong12345 <145076893+yhsong12345@users.noreply.github.com>
2023-09-15 10:46:15 +0900

    Update model.py

--- model.py
+++ model.py
@@ -59,37 +59,5 @@ class ResNet(nn.Module):
         o = self.linear(o)
 
         return o
-    
 
-class Plain(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(Plain, self).__init__()
-        self.in_planes = 16
-
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
-        self.linear = nn.Linear(64, num_classes)
-
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
-        layers=[]
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes*block.expansion
-        
-        return nn.Sequential(*layers)
-    
-    def forward(self, x):
-        o = F.relu(self.bn1(self.conv1(x)))
-        o = self.layer1(o)
-        o = self.layer2(o)
-        o = self.layer3(o)
-        o = F.avg_pool2d(o, o.size()[3])
-        o = o.view(o.size(0), -1)
-        o = self.linear(o)
-
-        return o
     
